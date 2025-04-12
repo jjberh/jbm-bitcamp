f0c634a9cea9bd6b4652fd8afc2853d33c2f6d95
jjberh <joshberhanu@gmail.com>
2025-04-12 00:42:08 -0400

    main

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,9 @@ from app import create_app
 
 app = create_app()
 
+@app.route("/")
+def hello():
+    return "Hello"
 # only used if running local
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+    #app.run(debug=True)
