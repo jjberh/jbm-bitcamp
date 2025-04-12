83d4658e26040e036cbbe38871e1d90680156ecc
jjberh <joshberhanu@gmail.com>
2025-04-12 02:52:51 -0400

    route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -16,6 +16,13 @@ def signup():
         return jsonify({"Missing email, username, or password"})
    
 
+@url.route("/users/")
+def users():
+    response = (
+        current_app.supabase.table("users").select("*").execute()
+    )
+
+    return response
 
 
 @url.route("/login")
