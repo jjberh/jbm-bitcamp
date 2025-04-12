12015a11b641e304b71af3f72f676e4c1990aea2
Tamunoopubo <m.taribi18@gmail.com>
2025-04-12 02:19:17 -0400

    Connecting our supa db

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
 
 def create_app():
 
     app = Flask(__name__)
+
+
+    #Connect to Supabase
+    supabase_url = os.getenv("DB_URL") #Uses our env var
+    supabase_key = os.getenv("DB_BACKEND_KEY")
+    app.supabase = create_client(supabase_url, supabase_key)
 
 
     return app
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, jsonify, current_app
 url = Blueprint("url", __name__)
 
 @url.route("/")
 def main():
     return "Hello"
+
+@url.route("/signup", methods = "GET")
 
 @url.route("/login")
 def login():
